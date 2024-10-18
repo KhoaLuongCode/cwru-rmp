5d698042e24c190a16d4a50373dd129ae5a3aa3b
Ruby-L-i-n <122896514+Ruby-L-i-n@users.noreply.github.com>
2024-10-17 20:20:15 -0400

    update search

--- CWRU_RMP_REACT/src/App.js
+++ CWRU_RMP_REACT/src/App.js
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react'
 import { supabase } from './supabaseClient'
 import Auth from './Auth'
 import Account from './Account'
+import Search from './Search'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 export default function Home() {
   const [session, setSession] = useState(null)
@@ -32,22 +34,38 @@ export default function Home() {
   }
 
   return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      {!session ? (
-        <Auth />
-      ) : session.user.email_confirmed_at ? (
-        <Account key={session.user.id} session={session} />
-      ) : (
-        <div>
-          <p>Please verify your email to access your account.</p>
-          <button
-            className="button block"
-            onClick={() => supabase.auth.signOut()}
-          >
-            Sign Out
-          </button>
-        </div>
-      )}
+    <div>
+      <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      <BrowserRouter>
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li><Link to="/search">About</Link></li>
+            </ul>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path="/search" element={<Search />} />
+        </Routes>
+        {!session ? (
+          <Auth />
+        ) : session.user.email_confirmed_at ? (
+          <Account key={session.user.id} session={session} />
+        ) : (
+          <div>
+            <p>Please verify your email to access your account.</p>
+            <button
+              className="button block"
+              onClick={() => supabase.auth.signOut()}
+            >
+              Sign Out
+            </button>
+          </div>
+        )}
+            </BrowserRouter>
+      </div>
     </div>
+
   )
 }
